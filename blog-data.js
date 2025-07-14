83bdf26d6b5db0addae4b674544bc3133eb43009
aanjelos <94786161+aanjelos@users.noreply.github.com>
2025-07-14 11:41:22 +0530

    v3 Blog Changes

--- blog-data.js
+++ blog-data.js
@@ -12,38 +12,38 @@
 // =================================================================
 
 const blogPosts = [
-      {
-    id: "sri-lanka-digital-tax-guide",
-    title: "The Upcoming 18% VAT in Sri Lanka: How It Affects Your Subscriptions",
-    date: "2025-07-10",
-    author: "Aanjelo Salgado",
-    image:
-      "blog/img/sri-lanka-digital-tax-guide.jpg",
-    image_alt: "Image showing v3 in a black background.",
-    url: "blog/sri-lanka-digital-tax-guide.html",
-    excerpt:
-      "Sri Lanka is preparing to implement an 18% VAT on digital services like Netflix, Spotify, and more. Learn which services will be affected and how to prepare your budget.",
-  },
-    {
+  {
     id: "kaasi-v3-changelog",
     title: "Kaasi v3 is Here: A New Home, New Features, and More!",
-    date: "2025-07-07",
+    date: "2025-07-15",
     author: "Aanjelo Salgado",
-    image:
-      "blog/img/kaasi-v3-changelog.jpg",
+    image: "blog/img/kaasi-v3-changelog.jpg",
     image_alt: "Image showing v3 in a black background.",
     url: "blog/kaasi-v3-changelog.html",
     excerpt:
       "Announcing the Kaasi v3 update! Discover our new home at kaasi.com.lk, a brand new website, and exciting new app features.",
   },
   {
+    id: "sri-lanka-digital-tax-guide",
+    title:
+      "The Upcoming 18% VAT in Sri Lanka: How It Affects Your Subscriptions",
+    date: "2025-07-10",
+    author: "Aanjelo Salgado",
+    image: "blog/img/sri-lanka-digital-tax-guide.jpg",
+    image_alt: "Image showing v3 in a black background.",
+    url: "blog/sri-lanka-digital-tax-guide.html",
+    excerpt:
+      "Sri Lanka is preparing to implement an 18% VAT on digital services like Netflix, Spotify, and more. Learn which services will be affected and how to prepare your budget.",
+  },
+
+  {
     id: "the-story-of-kaasi",
     title: "From a Spreadsheet to an App: The Story Behind Kaasi",
     date: "2025-07-01",
     author: "Aanjelo Salgado",
-    image:
-      "blog/img/the-story-of-kaasi.jpg",
-    image_alt: "Two laptops, with one showing the old spreadsheet and the other showing Kaasi UI.",
+    image: "blog/img/the-story-of-kaasi.jpg",
+    image_alt:
+      "Two laptops, with one showing the old spreadsheet and the other showing Kaasi UI.",
     url: "blog/the-story-of-kaasi.html",
     excerpt:
       "Discover the origin story of Kaasi, a personal finance app built for Sri Lankans out of a personal need for something better.",
